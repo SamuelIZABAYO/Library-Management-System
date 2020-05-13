7657d7c23313f15fc28fba1fbf7a53278f6ffd84
Crush <samizabayo1@gmail.com>
2020-05-13 11:39:19 +0200

    Library management System

---
+++ Library Manage/src/Model/ClientCategory.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author IZABAYO.S The Crushing Architect
+ */
+public enum ClientCategory {
+   STUDENT, STAFF, EXTERNAL;
+}
